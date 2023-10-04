3adf1b2ff23cba0e940782be90f7d7e1ce5d3c27
NickyFranklin <nafrankl@mtu.edu>
2023-10-04 11:36:23 -0400

    qsort sorts I guess

--- Project2/main.c
+++ Project2/main.c
@@ -14,30 +14,27 @@
 /* PROGRAM PURPOSE: To take in unsorted arrays and sort them concurrently    */
 /*---------------------------------------------------------------------------*/
 
-void fillArray(int *arr[]) {
-  int size;
-  scanf("%d", &size);
-  *arr = (int*) malloc((size+1) * sizeof(int));
-  *arr[0] = size;
-  for(int i = 1; i <= size; i++) {
+void fillArray(int *arr[], int size) {
+  *arr = (int*) malloc((size) * sizeof(int));
+  for(int i = 0; i < size; i++) {
     scanf("%d", &(*arr)[i]);
   }
 }
 
-void printArray(int arr[], int isQsort, int isX) {
+void printArray(int arr[], int isQsort, int isX, int size) {
   if(isQsort) {
-    printf("Input array for qsort has %d element:\n", arr[0]);
+    printf("Input array for qsort has %d element:\n", size);
   }
 
   else if(isX) {
-    printf("Input array x[] for merge has %d element:\n", arr[0]);
+    printf("Input array x[] for merge has %d element:\n", size);
   }
 
   else {
-    printf("Input array y[] for merge has %d element:\n", arr[0]);
+    printf("Input array y[] for merge has %d element:\n", size);
   }
   
-  for(int i = 1; i <= arr[0]; i++) {
+  for(int i = 0; i < size; i++) {
     printf("%d ", arr[i]);
   }
   printf("\n\n");
@@ -47,10 +44,16 @@ int main() {
   int *a;
   int *x;
   int *y;
-
-  fillArray(&a);
-  fillArray(&x);
-  fillArray(&y);
+  int sizeQ;
+  int sizeX;
+  int sizeY;
+  scanf("%d", &sizeQ);
+  printf("%d\n", sizeQ);
+  fillArray(&a, sizeQ);
+  scanf("%d", &sizeX);
+  fillArray(&x, sizeX);
+  scanf("%d", &sizeY);
+  fillArray(&y, sizeY);
   
   int ShmIDQSort;
   key_t keyQsort;
@@ -67,7 +70,7 @@ int main() {
   keyQsort = ftok("main.c", 'q');
   printf("*** MAIN: qsort shared memory key = %d\n", keyQsort);
   
-  ShmIDQSort = shmget(keyQsort, sizeof(int) * (a[0]+1), IPC_CREAT | 0666);
+  ShmIDQSort = shmget(keyQsort, sizeof(int) * (sizeQ), IPC_CREAT | 0666);
   if(ShmIDQSort < 0) {
     printf("Uh oh, the shared memory segment was not created\n");
     return 1;
@@ -81,7 +84,7 @@ int main() {
   keyx = ftok("main.c", 'x');
   printf("*** MAIN: merge x[] shared memory key = %d\n", keyx);
   
-  ShmIDx = shmget(keyx, sizeof(int) * (x[0]+1), IPC_CREAT | 0666);
+  ShmIDx = shmget(keyx, sizeof(int) * (sizeX), IPC_CREAT | 0666);
   if(ShmIDx < 0) {
     printf("Uh oh, the shared memory segment was not created\n");
     return 1;
@@ -95,7 +98,7 @@ int main() {
   keyy = ftok("main.c", 'y');
   printf("*** MAIN: merge y[] shared memory key = %d\n", keyy);
   
-  ShmIDy = shmget(keyy, sizeof(int) * (y[0]+1), IPC_CREAT | 0666);
+  ShmIDy = shmget(keyy, sizeof(int) * (sizeY), IPC_CREAT | 0666);
   if(ShmIDy < 0) {
     printf("Uh oh, the shared memory segment was not created\n");
     return 1;
@@ -109,21 +112,21 @@ int main() {
   //fillArray(qSortPtr);
   //fillArray(xPtr);
   //fillArray(yPtr);
-  for(int i = 0; i <= a[0]; i++) {
+  for(int i = 0; i < sizeQ; i++) {
     qSortPtr[i] = a[i];
   }
 
-  for(int i = 0; i <= x[0]; i++) {
+  for(int i = 0; i < sizeX; i++) {
     xPtr[i] = x[i];
   }
 
-  for(int i = 0; i <= y[0]; i++) {
+  for(int i = 0; i < sizeY; i++) {
     yPtr[i] = y[i];
   }
   
-  printArray(qSortPtr, 1, 0);
-  printArray(xPtr, 0, 1);
-  printArray(yPtr, 0, 0);
+  printArray(qSortPtr, 1, 0, sizeQ);
+  printArray(xPtr, 0, 1, sizeX);
+  printArray(yPtr, 0, 0, sizeY);
 
   printf("*** MAIN: about to spawn the process for qsort\n");
   printf("*** MAIN: about to spawn the process for merge\n");
@@ -137,15 +140,16 @@ int main() {
  
   //qsort
   if(cpid == 0) {
-    char *argv[5];
-    for(int i = 0; i < 5; i++) {
+    char *argv[6];
+    for(int i = 0; i < 6; i++) {
       argv[i] = (char*) malloc(100);
     }
     argv[0] = "./qsort";
-    sprintf(argv[1], "%d", 1);
-    sprintf(argv[2], "%d", qSortPtr[0]);
+    sprintf(argv[1], "%d", 0);
+    sprintf(argv[2], "%d", atoi(argv[1]) + sizeQ-1);
     sprintf(argv[3], "%d", ShmIDQSort);
-    argv[4] = NULL;
+    sprintf(argv[4], "%d", sizeQ);
+    argv[5] = NULL;
     execvp(argv[0], argv);
     printf("oopsies\n");
   }
--- Project2/qsort.c
+++ Project2/qsort.c
@@ -5,6 +5,31 @@
 #include <sys/shm.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
+
+void swap(int* a, int* b) {
+  int s = *a;
+  *a = *b;
+  *b = s;
+}
+
+int partition(int arr[], int low, int high) {
+  char buff[1000];
+  int pivot = arr[high];
+  pid_t cpid = getpid();
+  sprintf(buff, "   ### Q-PROC(%d): pivot element is a[%d] = %d\n", cpid, high, arr[high-1]); 
+  write(1, buff, strlen(buff));
+  int lowIndex = low - 1;
+
+  for(int i = low; i <= high; i++) {
+    if(arr[i] < pivot) {
+      lowIndex++;
+      swap(&arr[lowIndex], &arr[i]);
+    }
+  }
+  swap(&arr[lowIndex+1], &arr[high]);
+  return lowIndex+1;
+}
 
 int main(int argc, char** argv) {
   if(argc < 3) {
@@ -17,10 +42,69 @@ int main(int argc, char** argv) {
   cpid = getpid();
   char buff[1000];
   sprintf(buff, "   ### Q-PROC(%d): entering with a[%d..%d]\n   ", cpid,
-	  atoi(argv[1])-1, atoi(argv[2])-1);
+	  atoi(argv[1]), atoi(argv[2]));
   int length = strlen(buff);
-  int size = arr[0];
-  int j = 1;
+  int size = (atoi(argv[4]));
+  //printf("\n%d\n", size);
+  int j = atoi(argv[1]);
+  for(int i = length; i < 1000 && j < size ; i+=0) {
+    sprintf(buff+i, "%d ", arr[j]);
+    i = strlen(buff);
+    j++;
+  } 
+  sprintf(buff+strlen(buff), "\n");
+  write(1, buff, strlen(buff));
+  int low = atoi(argv[1]);
+  int high = atoi(argv[2]);
+  if(low < high) {
+    int part = partition(arr, atoi(argv[1]), atoi(argv[2]));
+    int status = 0;
+    pid_t wpid;
+    //int low = atoi(argv[1]);
+    //int high = atoi(argv[2]);
+    //printf("%d %d\n", low, part-1);
+    //printf("%d %d\n", part+1, high);
+    
+    cpid = fork();
+    
+    if(cpid == 0) {
+      char *args[6];
+      for(int i = 0; i < 6; i++) {
+	args[i] = (char*) malloc(100);
+      }
+      args[0] = "./qsort";
+      sprintf(args[1], "%d", atoi(argv[1]));
+      sprintf(args[2], "%d", (part-1));
+      sprintf(args[3], "%d", atoi(argv[3]));
+      sprintf(args[4], "%d", (part - low));
+      args[5] = NULL;
+      execvp(args[0], args);
+      printf("\nWhoops\n");
+    }
+    
+    cpid = fork();
+    
+    if(cpid == 0) {
+      char *args[6];
+      for(int i = 0; i < 6; i++) {
+	args[i] = (char*) malloc(100);
+      }
+      args[0] = "./qsort";
+      sprintf(args[1], "%d", (part + 1));
+      sprintf(args[2], "%d", high);
+      sprintf(args[3], "%d", atoi(argv[3]));
+      sprintf(args[4], "%d", (high - part+1) + 1);
+      args[5] = NULL;
+      execvp(args[0], args);
+      printf("\nWhoops\n");
+    }
+    while((wpid = wait(&status)) > 0);
+  }
+  sprintf(buff, "   ### Q-PROC(%d): section a[%d..%d] sorted\n   ", cpid,
+	  atoi(argv[1])-1, atoi(argv[2])-1);
+  length = strlen(buff);
+  size = atoi(argv[2]);
+  j = atoi(argv[1]);
   for(int i = length; i < 1000 && j <= size ; i+=0) {
     sprintf(buff+i, "%d ", arr[j]);
     i = strlen(buff);
@@ -29,6 +113,7 @@ int main(int argc, char** argv) {
   
   sprintf(buff+strlen(buff), "\n");
   write(1, buff, strlen(buff));
+  sprintf(buff, "   ### Q-PROC(%d): exists\n", cpid);
   shmdt(arr);
   exit(0);
 }
