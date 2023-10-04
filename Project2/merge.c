579d3c001df9ad66aa7aefa40d9a1e925747337c
NickyFranklin <nafrankl@mtu.edu>
2023-10-03 20:46:28 -0400

    Print statements done

--- Project2/merge.c
+++ Project2/merge.c
@@ -4,8 +4,44 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
-  write(1, "merge.c\n", sizeof("merge.c\n"));
+  if(argc < 3) {
+    printf("it messed up\n");
+    exit(1);
+  }
+  int *x;
+  int *y;
+  pid_t cpid;
+  x = shmat(atoi(argv[1]), NULL, 0);
+  y = shmat(atoi(argv[2]), NULL, 0);
+  cpid = getpid();
+  char buff[1000];
+  int sizeX = x[0];
+  int sizeY = y[0];
+  sprintf(buff, "      $$$ M-PROC(%d): x: %d y: %d\n      ", cpid, sizeX, sizeY);
+  int length = strlen(buff);
+  int j = 1;
+  for(int i = length; i < 1000 && j <= sizeX; i+=0) {
+    sprintf(buff+i, "%d ", x[j]);
+    i = strlen(buff);
+    j++;
+  }
+  sprintf(buff+strlen(buff), "\n      ");
+
+  length = strlen(buff);
+  j = 1;
+  for(int i = length; i < 1000 && j <= sizeY; i+=0) {
+    sprintf(buff+i, "%d ", y[j]);
+    i = strlen(buff);
+    j++;
+  }
+  
+  sprintf(buff+strlen(buff), "\n");
+  write(1, buff, strlen(buff));
+  shmdt(x);
+  shmdt(y);
   return 0;
 }
--- Project2/qsort.c
+++ Project2/qsort.c
@@ -4,8 +4,31 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
-  write(1, "qsort.c\n", sizeof("qsort.c\n"));
+  if(argc < 3) {
+    printf("it messed up \n");
+    exit(1);
+  }
+  int *arr;
+  pid_t cpid;
+  arr = shmat(atoi(argv[3]), NULL, 0);
+  cpid = getpid();
+  char buff[1000];
+  sprintf(buff, "   ### Q-PROC(%d): entering with a[%d..%d]\n   ", cpid,
+	  atoi(argv[1])-1, atoi(argv[2])-1);
+  int length = strlen(buff);
+  int size = arr[0];
+  int j = 1;
+  for(int i = length; i < 1000 && j <= size ; i+=0) {
+    sprintf(buff+i, "%d ", arr[j]);
+    i = strlen(buff);
+    j++;
+  }
+  
+  sprintf(buff+strlen(buff), "\n");
+  write(1, buff, strlen(buff));
+  shmdt(arr);
   exit(0);
 }
